9016b1f1e24c21c5350e62c47539fae532ab30c6
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-01-17 19:10:06 -0500

    gitignore updated

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+import secret_key
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -9,7 +10,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SECRET_KEY'] = secret_key.SECRET_KEY
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
